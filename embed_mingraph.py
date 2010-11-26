130fb66d90da608861a8018cb2395887c0352f34
Andrew Byrd <andrew@pruitt-igoe.(none)>
2010-11-26 18:04:21 +0100

    Kernels now handle blocks smaller than the set of all vertices.

--- embed_mingraph.py
+++ embed_mingraph.py
@@ -49,7 +49,7 @@ def net(X, Y) :
         try:
             g.add_edge(fs%o, fs%d, ElapseTime(w))
         except VertexNotFoundError:
-            print o,d,w,'skipped'
+            pass
     return g
 
 def remove_deadends(g) :
@@ -67,58 +67,17 @@ def remove_deadends(g) :
                 g.remove_vertex(v.label)
                 n += 1
 
-def apsp_cpu () :
-    # updating cost array needed for parallelisation?
-    # otherwise another thread might unset modified?
-    cost = np.empty((n_tasks, nv), dtype=np.float32)
-    cost.fill(np.inf)
-    modify = np.empty((n_tasks, nv), dtype=np.int8)
-    modify.fill(0)
-
-    # set cost of starting vertices to 0 and enqueue them
-    for t in range(n_tasks) : 
-        cost[t, t]   = 0
-        modify[t, t] = 1
-        
-    print "begin"
-    while modify.any() :
-        print '   modified costs: ', np.sum(modify, axis=1)
-        for i in range(nv) :
-            for t in range(n_tasks) :
-                if modify[t, i] == 0 : continue
-                #print "task %i vertex %i checking" % (t, i)
-                modify[t, i] = 0
-                oc = cost[t, i]
-                for ei in range(va[i], va[i+1]) :
-                    to = ea[ei]
-                    nc = wa[ei] + oc
-                    #print "vertex %i old %f new %f:" % (to, cost[t, to], nc),
-                    if cost[t, to] > nc : 
-                       #print "updated"
-                       cost[t, to] = nc
-                       modify[t, to] = 1 
-    print "end"
-    print cost
-    print modify
-    return cost
-
 def apsp_gpu (v_low, block_size) :
-    src = open('mingraph_kernels.cu').read()
-    mod = SourceModule(src, options=["--ptxas-options=-v"])
-    scatter = mod.get_function("scatter")
-    vertex = gpuarray.to_gpu(va)
-    edge   = gpuarray.to_gpu(ea)
-    weight = gpuarray.to_gpu(wa)
-    cost   = np.empty((nv, block_size), dtype=np.int32)
+    cost   = np.empty((block_size, nv), dtype=np.int32)
     modify = np.zeros_like(cost)
     cost.fill(9999)
     for i in range(block_size) :
-        cost  [v_low + i, i] = 0
-        modify[v_low + i, i] = 1   
-    for i in range(nv): # need better break condition
-        #be careful to get parameters right, otherwise mysterious segfaults ('kernel launch errors') will happen.        
-        # need v_low parameter
-        scatter(vertex, edge, weight, cuda.InOut(cost), cuda.InOut(modify), block=(block_size,1,1), grid=(nv,1))
+        cost  [i, v_low + i] = 0
+        modify[i, v_low + i] = 1   
+    #for i in range(nv): # need better break condition
+    while modify.any() :
+        #be careful to get parameters right, otherwise mysterious segfaults ('kernel launch errors') or worse will happen.        
+        scatter(np.int32(nv), vertex, edge, weight, cuda.InOut(cost), cuda.InOut(modify), block=(block_size,1,1), grid=(nv,1))
         pycuda.autoinit.context.synchronize()    
     return cost
 
@@ -143,16 +102,16 @@ ag.add_edge('15_9', '10_9', ElapseTime(5))
 ag.add_edge('15_9', '18_9', ElapseTime(5))
 ag.add_edge('18_9', '15_9', ElapseTime(5))
 
-ag.add_edge('9_5', '9_9', ElapseTime(3))
-ag.add_edge('9_9', '9_5', ElapseTime(3))
-ag.add_edge('9_9', '9_13', ElapseTime(3))
-ag.add_edge('9_13', '9_9', ElapseTime(3))
-ag.add_edge('9_13', '9_17', ElapseTime(3))
-ag.add_edge('9_17', '9_13', ElapseTime(3))
+#ag.add_edge('9_5', '9_9', ElapseTime(3))
+#ag.add_edge('9_9', '9_5', ElapseTime(3))
+#ag.add_edge('9_9', '9_13', ElapseTime(3))
+#ag.add_edge('9_13', '9_9', ElapseTime(3))
+#ag.add_edge('9_13', '9_17', ElapseTime(3))
+#ag.add_edge('9_17', '9_13', ElapseTime(3))
 
 remove_deadends(ag)
 
-v = list(ag.vertices)
+v  = list(ag.vertices)
 nv = len(v)
 ne = len(ag.edges)
 
@@ -183,14 +142,14 @@ print va
 print ea
 print wa
 
-n_tasks = nv
-cost = apsp_gpu(0, n_tasks)
-print cost
+# === END MAKE EDGELIST REPRESENTATION ===
 
-# embed
+
+# === EMBED ===
 DIM = 3
-coord  = np.random.rand(nv, DIM)
-force  = np.empty ((nv, DIM), dtype=np.float32)
+coord = np.random.rand(nv, DIM)
+force = np.zeros((nv, DIM))
+error = np.zeros((nv,    ))
 
 from enthought.mayavi import mlab
 X = np.zeros((19, 19))
@@ -200,77 +159,49 @@ S = np.zeros_like(X)
 mesh = mlab.mesh(X, Y, Z, scalars=S)
 mesh_source = mesh.mlab_source
 
+src = open('mingraph_kernels.cu').read()
+mod = SourceModule(src, options=["--ptxas-options=-v"])
+scatter = mod.get_function("scatter")
+vertex = gpuarray.to_gpu(va)
+edge   = gpuarray.to_gpu(ea)
+weight = gpuarray.to_gpu(wa)
+
 def display_coords(coord) :
     for label, index in vl.iteritems():
         x, y = label.split('_')
-        if DIM > 3:
-            X[x, y], Y[x, y], Z[x, y], S[x, y] = coord[index, :4]
-            S[x, y] = err[index]
-        else:
-            X[x, y], Y[x, y], Z[x, y] = coord[index]
-            #S[x, y] = np.sum(vel[index])
-            #S[x, y] = sum(abs(force[index]))
-            S[x, y] = err[index]
+        X[x, y], Y[x, y], Z[x, y] = coord[index, 0:3]
+        S[x, y] = error[index]
     mesh_source.set(x=X, y=Y, z=Z, scalars=S)
-    #outline.extent = mesh_source.extent
-    #mlab.view(focalpoint=coord[vl['10_10']])    
 
-err = np.zeros((nv,))
-
-def mds_iterate(coord, force, cost):
-    global err
+def mds_iterate():
+    BLOCK_SIZE = 128
+    global coord, force, cost, error
     force.fill(0)
-    err.fill(0)
-    for t in range(n_tasks) :
-        vector = coord - coord[t]
-        # IF YOU USE AN L1 METRIC, OF COURSE YOUR RESULTS WILL LOOK STRANGE
-        # L1 metric        
-        #dist = np.sum(np.abs(vector), axis=1)
-        # L2 metric
-        dist = np.sqrt(np.sum(vector * vector, axis=1))
-        # L4 metric 
-        #dist = np.sqrt(np.sqrt(np.sum(vector * vector * vector * vector, axis=1)))
-        adjust = (cost[t] / dist) - 1
-        adjust[t] = 0 # avoid NaNs, could use nantonum
-        err += np.abs(adjust)
-        #print 'task', t
-        #print coord[t]
-        #print coord
-        #print vector
-        #print dist
-        #print cost[t]
-        #print adjust
-        force += vector * adjust[:, np.newaxis]
-        #force += (vector * (adjust * (adjust > 0))[:, np.newaxis])
-        #print force
-    #vel /= 2
-    coord += force / n_tasks
-    err /= n_tasks
-    #coord += force / (n_tasks)
-    #stress = np.sum(np.sqrt(np.sum(force * force, axis=1))) / nv # actually this is kinetic energy, not stress.
-    # kinetic energy, peak to peak, mean, median, standard deviation
-    print np.sum(np.abs(force)), np.max(err), np.mean(err), np.std(err) 
-    #stress = np.sqrt(np.sum(adjust * adjust) / n_tasks)
-    #plot3s(coord, i)    
-    #i += 1
-    #if ke < 100 : mlab.show() # 1 percent RMS error
+    error.fill(0)
+    for v_low in range(0, nv, BLOCK_SIZE) :
+        block_size = min(BLOCK_SIZE, nv - v_low - 1) # choose full or partial block. account for zero-based indexing!
+        cost = apsp_gpu(v_low, block_size)
+        for i in range(block_size) :
+            vector = coord - coord[v_low + i]
+            # l2 metric
+            dist = np.sqrt(np.sum(vector * vector, axis=1))
+            adjust = (cost[i] / dist) - 1
+            adjust[v_low + i] = 0 # avoid NaNs, could use nantonum
+            error += abs(adjust)
+            force += vector * adjust[:, np.newaxis]
+    coord += force / nv
+    error /= nv
+    # kinetic energy and peak, mean, median, standard deviation of errors
+    print np.sum(np.abs(force)), np.max(error), np.mean(error), np.std(error) 
 
 @mlab.animate(delay=10)
 def mayavi_mds():
     while(True):
-        mds_iterate(coord, force, cost)
+        mds_iterate()
         display_coords(coord)
         yield
 
 mayavi_mds()
-# redo axes since extent start out as all zeros
-#axes = mlab.axes(extent=(-100, 100, -100, 100, -100, 100))
-#outline = mlab.outline()
 mlab.show()
-
-for c in coord :
-    for d in range(DIM) :
-        print c[d],
-    print
 
 
--- mingraph_kernels.cu
+++ mingraph_kernels.cu
@@ -2,18 +2,19 @@
 
 // Calculate all pairs shortest path.
 // after Okuyama, Ino, and Hagihara 2008.
-__global__ void scatter (int *vertex, int *edge, int *weight, int *cost, int *modify) {
-    int fromv_tindex = blockIdx.x + blockDim.x * threadIdx.x; // damn fortran ordering
-    if ( !modify[fromv_tindex] ) return;  // kill thread if this vertex was not changed in the last pass
-    int fromv_cost = cost[fromv_tindex];  // get current cost for this vertex
-    modify[fromv_tindex] = 0;
+__global__ void scatter (int nv, int *vertex, int *edge, int *weight, int *cost, int *modify) {
+    // Note: the kernel does not need to know the origin vertices - their costs are simply set to 0 
+    int fromv_rel_index = blockIdx.x + nv * threadIdx.x;
+    if ( !modify[fromv_rel_index] ) return;  // kill thread if this vertex was not changed in the last pass
+    int fromv_cost = cost[fromv_rel_index];  // get current cost for this vertex
+    modify[fromv_rel_index] = 0;
     int edge_index_low  = vertex[blockIdx.x];      // block number is vertex number (one vertex per block)
     int edge_index_high = vertex[blockIdx.x + 1];  // edges out of a vertex are contiguous
     for (int edge_index = edge_index_low; edge_index < edge_index_high; edge_index++) {
         int new_cost = fromv_cost + weight[edge_index];
-        int tov_tindex = edge[edge_index] + blockDim.x * threadIdx.x;
-        if (new_cost < atomicMin(cost + tov_tindex, new_cost)) { // atomicMin returns old value
-            modify[tov_tindex] = 1; // enqueue the modified vertex for the next round
+        int tov_rel_index = edge[edge_index] + nv * threadIdx.x;
+        if (new_cost < atomicMin(cost + tov_rel_index, new_cost)) { // atomicMin returns old value
+            modify[tov_rel_index] = 1; // enqueue the modified vertex for the next round
         }
     }
 }
